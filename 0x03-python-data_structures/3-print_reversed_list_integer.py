eb550e9ee74f11748628a2783ec52cc02e53cf42
DAVIESROTA <daviesrota@gmail.com>
2023-06-14 10:50:11 -0700

    data structures tasks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
-#!/usr/bi/python3
-
-def print_reversed_list_integer(my_list = []):
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for k in reversed(my_list):
-             print("{:d}".format(i))
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    length = len(sentence)
-    if length == 0:
-        result = (0, None)
-        return result
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
     else:
-        res = (length, sentence[0:11])
-        return res
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
